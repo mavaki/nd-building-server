55bc9db114c08828978c227a99bbb3059b9e5c2d
NKyhl <nolan@kyhl.com>
2024-12-10 04:53:41 +0000

    clean old server versions from repo

--- server.py
+++ server.py
@@ -5,6 +5,31 @@ from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import threading, subprocess, socket, json
+
+def register_with_catalog(name, port):
+    # Recall function every 60 seconds
+    t = threading.Timer(60.0, register_with_catalog, args=[name, port]).start()
+
+    # Catalog entry
+    entry = { "type" : "magic-polaroid",
+             "owner" : "mvankir2",
+             "port" : port,
+             "project" : name
+           }
+
+    # Set addr and define UDP socket
+    addr = ('catalog.cse.nd.edu', 9097)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+    # Send UDP packet to name server
+    sock.connect(addr)
+    sock.sendall(json.dumps(entry).encode())
+    sock.close()
+
+# Get ip and send to catalog server
+ip = subprocess.run(['dig', 'ANY','+short', '@resolver2.opendns.com', 'myip.opendns.com'], capture_output=True, text=True).stdout.strip()
+register_with_catalog(ip, '8080')
 
 # Load the building classifier model
 model = load_model("building_classifier_model.h5")
@@ -14,7 +39,7 @@ app = Flask(__name__)
 
 # Load class indices from the test generator
 def get_class_indices():
-    test_dir = '/home/ec2-user/training_images'  # Adjust this to your training directory
+    test_dir = '/home/ec2-user/training_images'
     test_datagen = ImageDataGenerator(rescale=1.0 / 255)
     test_generator = test_datagen.flow_from_directory(
         test_dir,
@@ -28,6 +53,11 @@ def get_class_indices():
 class_indices = get_class_indices()
 class_labels = {v: k for k, v in class_indices.items()}
 
+# Generate images folder and subfolders to store new training data
+IMAGE_FOLDER = '/home/ec2-user/images'
+for class_label in class_labels.values():
+    subfolder = os.path.join(IMAGE_FOLDER, class_label)
+    os.makedirs(subfolder, exist_ok=True)
 
 def predict_image(model, img_path):
     """Predict the class of the given image using the loaded model."""
@@ -78,8 +108,8 @@ def classify():
         os.remove(image_filename)
 
 @app.route('/submit', methods=['POST'])
-def submit_image():
-    """Save given image and label to directory."""
+def submit():
+    """Save given image and label to directory for future model training."""
     if 'image' not in request.files or 'label' not in request.form:
         return jsonify({'error': 'Request must contain image and label'}), 400
 
@@ -89,23 +119,20 @@ def submit_image():
     # Validate label
     if not label:
         return jsonify({'error': 'Label is required'}), 400
+    if label not in class_names.values():
+        return jsonify({'error': 'Class does not exist'}), 400
 
-    save_dir = '/home/ec2-user/images'
+    # Save image in appropriate class folder
+    save_dir = os.path.join(IMAGE_FOLDER, label)
     os.makedirs(save_dir, exist_ok=True)
 
     timestamp = str(time.time()).replace('.', '')
-    name = f"{os.path.splitext(image.filename)[0]}_{timestamp}"
-    image_filename = os.path.join(save_dir, f"{name}.jpg")
+    image_filename = os.path.join(save_dir, f"{timestamp}.jpg")
     image.save(image_filename)
 
-    label_filepath = os.path.join(save_dir, f"{name}.txt")
-    with open(label_filepath, 'w') as label_file:
-        label_file.write(label)
-
     return jsonify({
-        'message': 'Image and label saved successfully'
+        'message': 'Image added to training data'
     }), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
-
